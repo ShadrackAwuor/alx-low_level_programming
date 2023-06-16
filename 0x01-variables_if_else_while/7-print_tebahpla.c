a85edbce8b9d6c2a9726dcf494fe0884b42bc933
shadrackawuor <dintoz254@gmail.com>
2023-06-16 06:39:18 -0700

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print in reverse
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+	return(0);
+}
+
