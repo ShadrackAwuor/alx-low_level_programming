73f8e541d04d54c0599dbcdefc15ad95403201e9
shadrackawuor <dintoz254@gmail.com>
2023-06-20 07:50:59 -0700

    fibonacci 4

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
 
 /**
-
  * main - main block
-
  * Description: computes and prints the sum of all the multiples of 3 or
-
  * 5 below 1024 (excluded), followed by a new line
-
  * Return: 0
-
  */
 
 int main(void)
