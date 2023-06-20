438602c1376ce091b30a719902b24dce4d62b6e9
shadrackawuor <dintoz254@gmail.com>
2023-06-20 04:10:51 -0700

    alpha10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,9 +10,9 @@ void print_alphabet_x10(void)
 	char c;
 	int i = 0;
 
-	for (i = 1; i < 10; i++)
+	for (i = 1; i <= 9; i++)
 	{
-		for (c = 'a'; c = 'z'; c++)
+		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
 		}
