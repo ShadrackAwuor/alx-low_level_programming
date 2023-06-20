f2787e11bdce6bd417930a7320a3f63ec60e528e
shadrackawuor <dintoz254@gmail.com>
2023-06-20 02:54:57 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - prints alphabet in lower case
+ *
+ * Return - nothing, null
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar('c');
+		_putchar('\n');
+	}
+
+	return (0);
+}
+
