724b7a50a352d2fde280f5a4397428f357cdf913
shadrackawuor <dintoz254@gmail.com>
2023-06-22 13:33:11 -0700

    main2

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-  * main - Tests function for positive or negative
+  * main - Test function for positive or negative
   * Return: 0
   */
 
@@ -14,4 +14,3 @@ positive_or_negative(i);
 
 return (0);
 }
-
