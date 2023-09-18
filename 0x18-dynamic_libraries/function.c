dfcf7ba6614f0cace2a69d75e847f482c2a88eeb
shadrackawuor <dintoz254@gmail.com>
2023-09-18 12:11:42 -0700

    dynamic libraries

---
+++ 0x18-dynamic_libraries/function.c
@@ -0,0 +1,102 @@
+#include "main.h"
+#include <stddef.h>
+
+int _putchar(char c) {
+    /* Your implementation here */
+    return 0;
+}
+
+int _islower(int c) {
+    /* Your implementation here */
+    return 0;
+}
+
+int _isalpha(int c) {
+    /* Your implementation here */
+    return 0;
+}
+
+int _abs(int n) {
+    /* Your implementation here */
+    return 0;
+}
+
+int _isupper(int c) {
+    /* Your implementation here */
+    return 0;
+}
+
+int _isdigit(int c) {
+    /* Your implementation here */
+    return 0;
+}
+
+int _strlen(char *s) {
+    /* Your implementation here */
+    return 0;
+}
+
+void _puts(char *s) {
+    /* Your implementation here */
+}
+
+char *_strcpy(char *dest, char *src) {
+    /* Your implementation here */
+    return dest;
+}
+
+int _atoi(char *s) {
+    /* Your implementation here */
+    return 0;
+}
+
+char *_strcat(char *dest, char *src) {
+    /* Your implementation here */
+    return dest;
+}
+
+char *_strncat(char *dest, char *src, int n) {
+    /* Your implementation here */
+    return dest;
+}
+
+char *_strncpy(char *dest, char *src, int n) {
+    /* Your implementation here */
+    return dest;
+}
+
+int _strcmp(char *s1, char *s2) {
+    /* Your implementation here */
+    return 0;
+}
+
+char *_memset(char *s, char b, unsigned int n) {
+    /* Your implementation here */
+    return s;
+}
+
+char *_memcpy(char *dest, char *src, unsigned int n) {
+    /* Your implementation here */
+    return dest;
+}
+
+char *_strchr(char *s, char c) {
+    /* Your implementation here */
+    return NULL;
+}
+
+unsigned int _strspn(char *s, char *accept) {
+    /* Your implementation here */
+    return 0;
+}
+
+char *_strpbrk(char *s, char *accept) {
+    /* Your implementation here */
+    return NULL;
+}
+
+char *_strstr(char *haystack, char *needle) {
+    /* Your implementation here */
+    return NULL;
+}
+
